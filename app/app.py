70eecf8ce0c5dc27d33ef5407986e6b4c2e2fe86
Murali <singampallimurali03@gmail.com>
2026-06-24 21:42:47 +0530

    Kubernetes deployment strategies and ingress

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "application": "release-lab",
+        "version": os.getenv("VERSION", "v2"),
+        "message": "New - release!"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
